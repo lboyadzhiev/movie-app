180f35dc3bd5c6b0e791cbd9a7b4ea8809ce20ed
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-04-19 08:42:44 +0300

    nav - functionality

--- src/app.js
+++ src/app.js
@@ -1,8 +1,34 @@
 import page from '../node_modules/page/page.mjs';
 import { render } from '../node_modules/lit-html/lit-html.js';
 
-import * as api from './api/api.js';
+import { homePage } from './views/home.js';
 
-api.settings.host = 'https://parseapi.back4app.com/';
+// import * as api from './api/api.js';
 
-window.api = api;
+// api.settings.host = 'https://parseapi.back4app.com/';
+
+const main = document.getElementById('container');
+setUserNav();
+
+page('/', decorateContext, homePage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = content => render(content, main);
+    ctx.setUserNav = setUserNav;
+
+    next();
+}
+
+function setUserNav() {
+    const token = sessionStorage.getItem('authToken');
+
+    if (token != null) {
+        document.querySelector('#user').style.display = 'block';
+        document.querySelector('#quest').style.display = 'none';
+    } else {
+        document.querySelector('#user').style.display = 'none';
+        document.querySelector('#quest').style.display = 'block';
+    }
+}
