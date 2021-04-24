a9b4d0149b3f5966b337b5a2e02909a41425a803
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-04-24 16:54:59 +0300

    catalog page

--- src/api/data.js
+++ src/api/data.js
@@ -1,39 +1,12 @@
 import * as api from './api.js';
 
-api.settings.host = 'https://parseapi.back4app.com';
+const host = 'https://parseapi.back4app.com';
+api.settings.host = host;
 
 export const login = api.login;
 export const register = api.register;
 export const logout = api.logout;
 
-function createPointer(name, id) {
-    return {
-        _type: 'Pointer',
-        className: name,
-        objectId: id,
-    };
-}
-
 export async function getMovies() {
-    return await api.get(host + '/classes/MyCustomClassName');
-}
-
-export async function getMovie(id) {
-    return await api.get(host + '/classes/MyCustomClassName' + id);
-}
-
-export async function createMovie(movie) {
-    const userId = sessionStorage.getItem('userId');
-    const body = Object.assign({}, movie, {
-        owner: createPointer('_User', userId),
-    });
-    return await api.post(host + 'classes/MyCustomClassName', body);
-}
-
-export async function updateMovie(id, movie) {
-    return await api.put(host + 'classes/MyCustomClassName' + id, movie);
-}
-
-export async function deleteMovie(id) {
-    return await api.del(host + 'classes/MyCustomClassName' + id);
+    return await api.get(host + '/classes/Movie');
 }
--- src/views/catalog.js
+++ src/views/catalog.js
@@ -1,6 +1,8 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-const catalogTemplate = () => html`
+import { getMovies } from '../api/data.js';
+
+const catalogTemplate = movies => html`
     <section class="allMovies" id="cinema">
         <h1 class="heading-primary">All Movies</h1>
         <form action="" class="form search-form" method="GET">
@@ -9,29 +11,28 @@ const catalogTemplate = () => html`
             <input type="submit" class="btn" value="search" />
         </form>
 
-        <div class="movies">
-            <div class="movies__movie">
-                <h2 class="heading-secondary">Title: Us (2019)</h2>
-                <img
-                src=https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_UX140_CR0,0,140,209_AL_.jpg
-                alt="movie img" class="movies__img">
-                <p class="movies__tickets">Available Tickets: 50</p>
-
-                <div class="movies__btn-group">
-                    <button class="movies__btn btn">
-                        <a href="#">Buy Ticket</a>
-                    </button>
-                    <button class="movies__btn btn">
-                        <a href="#">Details</a>
-                    </button>
-                </div>
-            </div>
-        </div>
+        <div class="movies">${Object.values(movies).map(movieTemplate)}</div>
     </section>
 `;
 
-export async function catalogPage(ctx) {
-    ctx.render(catalogTemplate());
+const movieTemplate = movie => html`
+    <div class="movies__movie">
+        <h2 class="heading-secondary">${movie.title}</h2>
+        <img ${movie.imgUrl} alt="movie img" class="movies__img" />
+        <p class="movies__tickets">Available Tickets: ${movie.tickets}</p>
 
-    ctx.setUserNav();
+        <div class="movies__btn-group">
+            <button class="movies__btn btn">
+                <a href="#">Buy Ticket</a>
+            </button>
+            <button class="movies__btn btn">
+                <a href="/details">Details</a>
+            </button>
+        </div>
+    </div>
+`;
+
+export async function catalogPage(ctx) {
+    const movies = await getMovies();
+    ctx.render(catalogTemplate(movies));
 }
