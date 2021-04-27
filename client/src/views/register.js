48c5665d3b4dcf90dbc2a8c8954ed67708b1b1d6
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-04-27 10:17:32 +0300

    registration form html css changes

--- client/src/views/register.js
+++ client/src/views/register.js
@@ -1,3 +1,20 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { register } from '../api/data.js';
+
+const registerTemplate = () => html`
+    <section class="form-section">
+        <h1 class="heading-primary">Register</h1>
+        <form class="form">
+            <label>Email</label>
+            <input class="form-item" type="text" name="email" />
+            <label>Password</label>
+            <input class="form-item" type="password" name="password" />
+            <label>Repeat Password</label>
+            <input class="form-item" type="password" name="repeatPassword" />
+            <input class="btn" type="submit" value="Register" />
+        </form>
+    </section>
+`;
 export async function registerPage(ctx) {
-    console.log('Register Page');
+    ctx.render(registerTemplate());
 }
