fc466baa86fb7aabe4bb3614b557c7b7472e40f7
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-04-28 09:04:21 +0300

    implement registration

--- client/src/views/register.js
+++ client/src/views/register.js
@@ -1,10 +1,10 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 import { register } from '../api/data.js';
 
-const registerTemplate = () => html`
+const registerTemplate = onSubmit => html`
     <section class="form-section">
         <h1 class="heading-primary">Register</h1>
-        <form class="form">
+        <form @submit=${onSubmit} class="form">
             <label>Email</label>
             <input class="form-item" type="text" name="email" />
             <label>Password</label>
@@ -15,6 +15,28 @@ const registerTemplate = () => html`
         </form>
     </section>
 `;
+
 export async function registerPage(ctx) {
-    ctx.render(registerTemplate());
+    ctx.render(registerTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const rePass = formData.get('repeatPassword');
+
+        if (email == '' || password == '' || rePass == '') {
+            return alert('All fields are required!');
+        }
+
+        if (password != rePass) {
+            return alert("Passwords don't match!");
+        }
+
+        await register(email, password);
+
+        ctx.page.redirect('/movies');
+    }
 }
