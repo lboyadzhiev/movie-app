1184c107cc4730e8b1a13a115bcbf6b8df0aaa93
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-04-28 09:37:44 +0300

    errorMessage

--- client/src/views/register.js
+++ client/src/views/register.js
@@ -1,7 +1,7 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 import { register } from '../api/data.js';
 
-const registerTemplate = onSubmit => html`
+const registerTemplate = (onSubmit, errMrg) => html`
     <section class="form-section">
         <h1 class="heading-primary">Register</h1>
         <form @submit=${onSubmit} class="form">
@@ -12,6 +12,7 @@ const registerTemplate = onSubmit => html`
             <label>Repeat Password</label>
             <input class="form-item" type="password" name="repeatPassword" />
             <input class="btn" type="submit" value="Register" />
+            ${errMrg ? html`<p class="errMsg">${errMrg}</p>` : ''}
         </form>
     </section>
 `;
@@ -28,11 +29,15 @@ export async function registerPage(ctx) {
         const rePass = formData.get('repeatPassword');
 
         if (email == '' || password == '' || rePass == '') {
-            return alert('All fields are required!');
+            return ctx.render(
+                registerTemplate(onSubmit, 'All fields are required!!!')
+            );
         }
 
         if (password != rePass) {
-            return alert("Passwords don't match!");
+            return ctx.render(
+                registerTemplate(onSubmit, "Passwords don't match!!!")
+            );
         }
 
         await register(email, password);
