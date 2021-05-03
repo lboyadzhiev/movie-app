68499a568da556dc30496aaf573b5caf9511d71a
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-05-03 15:00:38 +0300

    create page

--- client/src/views/create.js
+++ client/src/views/create.js
@@ -1,3 +1,45 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { createMovie } from '../api/data.js';
+
+const createTemplate = onSubmit => html`
+    <section class="form-section">
+        <h1 class="heading-primary">Create movie</h1>
+        <form @submit=${onSubmit} class="form">
+            <label>Title</label>
+            <input class="form-item" type="text" name="title" />
+            <label>Image Url</label>
+            <input class="form-item" type="text" name="imageUrl" />
+            <label>Description</label>
+            <textarea
+                class="form-item"
+                type="text"
+                name="description"
+            ></textarea>
+            <label>Genres</label>
+            <input class="form-item" type="text" name="genres" />
+            <input class="btn" type="submit" value="Create" />
+        </form>
+    </section>
+`;
 export async function createPage(ctx) {
-    console.log('create page');
+    ctx.render(createTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        const title = formData.get('title');
+        const img = formData.get('imageUrl');
+        const desc = formData.get('description');
+        const genre = formData.get('genres');
+
+        if (title == '' || img == '' || desc == '' || genre == '') {
+            return ctx.render(onSubmit, 'All fields are required!!!');
+        }
+
+        await createMovie({ title, img, desc, genre });
+
+        ctx.page.redirect('/my-movies');
+    }
 }
--- client/src/views/edit.js
+++ client/src/views/edit.js
@@ -1,3 +1,45 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { getMovieById, editMovie } from '../api/data.js';
+
+const editTemplate = (movie, onSubmit) => html`
+    <div id="editMovie">
+        <h1>Edit movie</h1>
+        <form @submit=${onSubmit}>
+            <label>Title</label>
+            <input type="text" name="title" value="Us (2019)" id="editTitle" />
+            <label>Image Url</label>
+            <input
+                type="text"
+                name="imageUrl"
+                id="editImage"
+                value="https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_UX140_CR0,0,140,209_AL_.jpg"
+            />
+            <label>Description</label>
+            <textarea type="text" id="editDescription" name="description">
+A family's serenity turns to chaos when a group of doppelgängers begins to terrorize them.</textarea
+            >
+            <label>Genres</label>
+            <input
+                type="text"
+                name="genres"
+                value="Horror,Triller"
+                id="editGenres"
+            />
+            <label>Available Tickets</label>
+            <input type="number" name="tickets" value="50" id="editTickets" />
+            <input type="submit" value="Edit" />
+        </form>
+    </div>
+`;
 export async function editPage(ctx) {
-    console.log('edit page', ctx.params.id);
+    const id = ctx.params.id;
+    const movie = await getMovieById(id);
+
+    ctx.render(editTemplate(movie, onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+    }
 }
